9c6942b6080c155f5065db1f0a39584799de98fb
zloopk09 <zloopk09@gmail.com>
2020-05-28 19:56:26 +0800

    引入arouter

--- fdn_core/src/main/java/top/zloop/fdn/core/RouterMap.java
+++ fdn_core/src/main/java/top/zloop/fdn/core/RouterMap.java
@@ -3,12 +3,12 @@ package top.zloop.fdn.core;
 public class RouterMap {
 
     // arouter定义必须/开头 包含两个// 例如/app/order
-    public static final String URL_MAIL="/activity/mail";
-    public static final String URL_USER="/activity/user";
-    public static final String URL_Login="/activity/login";
+    public static final String URL_MAIL="/mail/mail";
+    public static final String URL_USER="/account/user";
+    public static final String URL_Login="/login/login";
 
 
-    public static final String URL_USER_SERVICE="/method/userservice";
+    public static final String URL_USER_SERVICE="/accountservice/userservice";
 
 
 }
